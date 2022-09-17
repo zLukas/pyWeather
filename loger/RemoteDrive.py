678cc2afb16278fc9abfd9ed35eefbf11db5e84a
Lukas <xlukas9@gmail.com>
2022-09-17 19:19:50 +0200

    submodule fix

---
+++ loger/RemoteDrive.py
@@ -0,0 +1,9 @@
+class RemoteDrive():
+    def __init__(self):
+        self.push_script = 'pyDrive/pyDrive.py'
+        
+    def push_file(remote_dir, file):
+        # check if script exist
+        # if yes push data 
+        # return script code
+
---
+++ tests/loger_tests/test_RemoteDrive.py
@@ -0,0 +1,23 @@
+
+import sys
+from loger import RemoteDrive
+
+
+
+def push_data_script_not_exist():
+    pass
+
+def push_data_script_return_error():
+    pass
+
+def push_data_remote_folder_exist():
+    pass
+
+def push_data_remote_folder_not_exist():
+    pass
+
+def push_data_file_already_exist():
+    pass
+
+def
+
