82eebce918ad5db448bb5161888adbdc895e67d9
Lukas <xlukas9@gmail.com>
2023-07-04 20:00:54 +0200

    file renaming

--- sqlite/measurement_data_dase.py
+++ sqlite/measurement_data_dase.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-class MeasurementDataBase :
+class MeasurementDataBase:
 
     def __init__(self):
 
@@ -19,6 +19,7 @@ class MeasurementDataBase :
             self._sqlite3 = sqlite3.connect(self._dbName)
             self.status = f"connected"
             self._sqlite3.execute(CREATE_QUERY)
+            self._sqlite3.close()
         except: 
             print(" cannot create database ")
 
@@ -35,6 +36,7 @@ class MeasurementDataBase :
                 values = (items['datetime'], items['sensor_id'], items['temperature'], items['humidity'], items['pressure'])
                 self._sqlite3.execute(putQuery, values)
                 self._sqlite3.commit()
+                self._sqlite3.close()
 
 if __name__ == "__main__":
     if os.path.exists("measurements.db"):
