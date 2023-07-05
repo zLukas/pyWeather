2854a3795243eb9fab7c0a1f9b363c8449e0ef65
Lukas <xlukas9@gmail.com>
2023-07-05 17:30:51 +0200

    flask app renamed to frontend

--- frontend/routes.py
+++ frontend/routes.py
@@ -4,8 +4,7 @@ from flask import render_template
 from flask import redirect
 from app.forms import MeasurementForm
 
-
-sensor_data = None
+sensor_obj = None
 
 @app.route("/", methods=["GET"])
 def go_to_measurements():
@@ -13,9 +12,9 @@ def go_to_measurements():
 
 @app.route("/measurements", methods=["GET"])
 def measurements():
-        global sensor_data
+        global sensor_obj
         form = MeasurementForm()
-        form.humidityBox.data=f"{sensor_data.humidity:.1f} %"
-        form.pressureBox.data= f"{sensor_data.pressure:.1f} hPa"
-        form.temperatureBox.data = f"{sensor_data.temperature:.1f} C"
+        form.humidityBox.data=f"{sensor_obj.humidity:.1f} %"
+        form.pressureBox.data= f"{sensor_obj.pressure:.1f} hPa"
+        form.temperatureBox.data = f"{sensor_obj.temperature:.1f} C"
         return render_template('measurements.html', form=form)
