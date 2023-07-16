e2706d175c52ecff984b2516ccb5b4e4c0ee683d
Lukas <xlukas9@gmail.com>
2023-07-16 13:39:59 +0200

    set callback function added

--- api/app.py
+++ api/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
-form flask import redirect
+from flask import redirect
 from .config import Config
 
 
 app = Flask("station")
 app.config.from_object(Config)
 
-bme_callback = None
+sensor_callback = None
+
+def set_sensor_callback(callback):
+    global sensor_callback
+    sensor_callback = callback
 
 @app.route("/", methods=["GET"])
 def redirect_to_measurements():
@@ -14,9 +18,10 @@ def redirect_to_measurements():
 
 @app.route("/measurements", methods=["GET"])
 def get_measurements():
-    global bme_callback
-    response = {"error": "calback function is not defined"}
-    if bme_callback is not None \
-        and bme_callback.return_type is not None:
-        response = bme_callback.function()
+    response = {}
+    if sensor_callback is not None \
+        and sensor_callback.return_type is not None:
+        response = sensor_callback.function()
+    else:
+        response = {"error": "calback function is not defined, or return type is None"}
     return response
--- station.py
+++ station.py
@@ -2,11 +2,11 @@ from ast import Call
 from threading import Thread
 from sensor import Bme
 from api import app
-from api import bme_callback
+from api import set_sensor_callback
 from api.callback import Callback
 
 bme = Bme()
-bme_callback = Callback(bme.read_formated, dict)
+set_sensor_callback(Callback(bme.read_formated, dict))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
