47fc9eeb42e140f2ca2fa8cad9ee7e8423189b03
Lukas <xlukas9@gmail.com>
2023-07-16 11:03:07 +0200

    add station file

--- station.py
+++ station.py
@@ -4,17 +4,19 @@ from datetime import datetime as dt
 import schedule
 
 bme = Bme()
-sqlite  = SqliteDataBase()
+sqlite = SqliteDataBase()
+
 
 def read_and_save_measurements():
     data = bme.read_measurements()
     sqlite.PutItem({
-        "datetime" : dt.now().strftime("%m/%d/%Y-%H:%M:%S"),
-        "sensor_id" : "1",
+        "datetime": dt.now().strftime("%m/%d/%Y-%H:%M:%S"),
+        "sensor_id": "1",
         "temperature": data.temperature,
         "humididty": data.humidity,
         "pressure": data.pressure
     })
-	
+
+
 if __name__ == "__main__":
     schedule.every().hour.at(":00").do(read_and_save_measurements)
