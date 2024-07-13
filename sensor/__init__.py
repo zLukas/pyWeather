69956b749837b227ce0d879f58d8e5a4bc6c5d2c
zLukas <xlukas9@gmail.com>
2024-07-13 14:45:52 +0200

    implement Waveshare Environmental Hat

--- sensor/__init__.py
+++ sensor/__init__.py
@@ -1,1 +1,24 @@
-from .sensor import Bme
+import time
+from .bme280 import BME280
+from .ltr390 import LTR390
+from .tsl2591 import TSL2591
+from .spg40 import SGP40
+
+
+
+class SensorHat():
+    def __init__(self):
+        self.bme280 = BME280()
+        self.ltr390 = LTR390()
+        self.tsl2591 = TSL2591()
+        self.spg40  = SGP40()
+
+    def getBme280Data(self):
+        return  self.bme280.read_formated()
+    def getLtr390Data(self):
+        return self.ltr390.UVS()
+    def getTsl2591Data(self):
+        return self.tsl2591.Lux()
+    def getSgp40Data(self):
+        temperature, humidity, _ = self.getBme280Data()
+        return self.spg40.getVocNoxIndex(temperature, humidity)
--- sensor/bme280.py
+++ sensor/bme280.py
@@ -3,8 +3,9 @@ import bme280
 from time import sleep
 from datetime import datetime as dt
 
+# temperature, humidity, pressure
 
-class Bme():
+class BME280():
     def __init__(self):
         self.__port = 1
         self.__i2c_address = 0x76
@@ -18,8 +19,4 @@ class Bme():
 
     def read_formated(self):
         data = self.read_raw()
-        return {"datetime": dt.now().strftime("%m/%d/%Y-%H:%M:%S"),
-                "temperature": f"{round(data.temperature,2)}",
-                "humidity": f"{round(data.humidity,2)}",
-                "pressure": f"{round(data.pressure,2)}"
-                }
+        return round(data.temperature,2), round(data.humidity,2), round(data.pressure,2)
