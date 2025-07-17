4d614fda928d75a164e4d8a6d6b0d209dd030add
zLukas <xlukas9@gmail.com>
2025-07-17 16:00:21 +0200

    add html page

--- api/app.py
+++ api/app.py
@@ -1,28 +1,66 @@
-from telnetlib import BM
-from flask import Flask
+# from telnetlib import BM
+# from flask import Flask
+# from flask import redirect
+# from flask import jsonify
+# from .config import Config
+# from sensor import SensorHat
+
+# app = Flask("station")
+# app.config.from_object(Config)
+# #separate api and sensor, objects
+# hat = SensorHat()
+
+
+# def set_sensor_callback(callback):
+#     global sensor_callback
+#     sensor_callback = callback
+
+# @app.route("/", methods=["GET"])
+# def redirect_to_measurements():
+#     return redirect("/measurements")
+
+# @app.route("/measurements", methods=["GET"])
+# def get_measurements():
+#     return hat.getData()
+
+
+# @app.route("/status", methods=["GET"])
+# def heartbeat():
+#     return {"status": "I'm Alive"}
+
+
+
+from flask import Flask, render_template, jsonify
 from flask import redirect
+from flask import Response
+from threading import Thread
+import time
 from .config import Config
 from sensor import SensorHat
 
 app = Flask("station")
 app.config.from_object(Config)
-#separate api and sensor, objects
 hat = SensorHat()
 
+latest_data = {}
 
-def set_sensor_callback(callback):
-    global sensor_callback
-    sensor_callback = callback
+def sensor_reader():
+    global latest_data
+    while True:
+        latest_data = hat.getData()
+        time.sleep(60)
+
+# Start background thread to read sensor data every minute
+Thread(target=sensor_reader, daemon=True).start()
 
 @app.route("/", methods=["GET"])
-def redirect_to_measurements():
-    return redirect("/measurements")
+def index():
+    return render_template("index.html", data=latest_data)
 
-@app.route("/measurements", methods=["GET"])
+@app.route("/api/v1/raw", methods=["GET"])
 def get_measurements():
-    return hat.getData()
+    return jsonify(latest_data)
 
-
-@app.route("/status", methods=["GET"])
+@app.route("/api/v1/status", methods=["GET"])
 def heartbeat():
     return {"status": "I'm Alive"}
